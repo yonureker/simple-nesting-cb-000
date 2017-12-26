57e370a94e31a9ce45a20886aaecfbc3e4cb0c76
Onur Eker <yonureker@gmail.com>
2017-12-26 07:04:48 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -85,8 +85,8 @@ def adding_matz
 
   programmer_hash[:yukihiro_matsumoto] =
 	 {
-    		known_for => "Ruby",
-    		languages => ["LISP", "C"]
+    		:known_for => "Ruby",
+    		:languages => ["LISP", "C"]
   	}
 
 end
