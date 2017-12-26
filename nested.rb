cd27a542c53c37084cf99729a4f87496147699f3
Onur Eker <yonureker@gmail.com>
2017-12-26 07:11:08 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -111,6 +111,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
 		 programmer_hash[:alan_kay][:known_for] = "GUI"
+		 return programmer_hash[:alan_kay]
 
 end
 
