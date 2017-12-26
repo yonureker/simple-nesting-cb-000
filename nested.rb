071ab23b75ac810db035f1c623c6719d7b6558c8
Onur Eker <yonureker@gmail.com>
2017-12-26 05:32:36 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -87,7 +87,7 @@ programmer_hash[:yukihiro_matsumoto] = {
   :known_for => "Ruby",
   :languages => ["LISP", "C"]
 }
-return programmer_hash[:yukihiro_matsumoto]
+puts programmer_hash
 end
 
 def changing_alan
